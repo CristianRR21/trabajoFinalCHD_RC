edc05b28fff467bd27f46d40fcbca3228c45ee9a
Cristian Lenin Roblero <cristianroblero00@gmail.com>
2025-06-28 22:58:11 -0500

    Configuracion de modelos-plantilla

--- Aplicaciones/gestionArriendo/models.py
+++ Aplicaciones/gestionArriendo/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
+#from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    telefono = models.CharField(max_length=10)
+
+    direccion = models.TextField()
+    bloqueado = models.BooleanField(default=False)
+
+class TipoHabitacion(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.TextField()
+
+class Publicacion(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    titulo = models.TextField()
+    precio = models.DecimalField(max_digits=5, decimal_places=3)
+    descripcion = models.TextField()
+    tipohabitacion = models.ForeignKey(TipoHabitacion, on_delete=models.PROTECT)
+    latitud = models.DecimalField(max_digits=9, decimal_places=6)
+    longitud = models.DecimalField(max_digits=9, decimal_places=6)
+    fechacreacion = models.DateField(auto_now_add=True)
+    estado = models.TextField(default='ACTIVO') 
+
+class Fotografia(models.Model):
+    id = models.AutoField(primary_key=True)
+    publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='publicaciones', null=True, blank=True)
+    orden = models.IntegerField()
+
+class HistorialEliminacion(models.Model):
+    id = models.AutoField(primary_key=True)
+    publicacion = models.ForeignKey(Publicacion, on_delete=models.SET_NULL, null=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True)
+    motivo = models.TextField()
+    fechaeliminacion = models.DateField(auto_now_add=True)
+
+class BloqueoUsuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    motivo = models.TextField()
+    fecha_bloqueo = models.DateField(auto_now_add=True)
+
+class Favorito(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE)
+    fechaagregado = models.DateField(auto_now_add=True)
+
+class Calificacion(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario= models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE)
+    puntuacion = models.IntegerField()
+    fecha = models.DateField(auto_now_add=True)
+
+class HistorialVisualizacion(models.Model):
+    id = models.AutoField(primary_key=True)
+    publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE)
+    fecha = models.DateField(auto_now_add=True)
+    usuario= models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    
+
+class ComentarioPublicacion(models.Model):
+    id = models.AutoField(primary_key=True)
+    publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    texto = models.TextField()
+    fecha = models.DateField(auto_now_add=True)
